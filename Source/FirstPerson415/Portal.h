5d27d14fc4e630c3d0597361b804b5a69d58afcf
ChaseTingle <chase.tingle@snhu.edu>
2026-02-08 05:46:31 -0600

    Module 5 activity and stepping stone, portals.

---
+++ Source/FirstPerson415/Portal.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Components/BoxComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Portal.generated.h"
+
+class FirstPerson415Character;
+
+UCLASS()
+class FIRSTPERSON415_API APortal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APortal();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		USceneCaptureComponent2D* sceneCapture;
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UArrowComponent* rootArrow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UTextureRenderTarget2D* renderTarget;
+
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		UBoxComponent* boxComp;
+
+	UPROPERTY(EditAnywhere)
+		APortal* OtherPortal;
+
+	UPROPERTY(EditAnywhere)
+		UMaterialInterface* mat;
+
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void SetBool(AFirstPerson415Character* playerChar);
+
+	UFUNCTION()
+		void UpdatePortals();
+
+};
